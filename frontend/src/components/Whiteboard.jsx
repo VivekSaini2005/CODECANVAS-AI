bfb9d564a077c3dfeaef66e95fddc80e36299de6
Vivek Saini <sainivivek5392@gmail.com>
2026-03-17 00:11:22 +0530

    compiler done

--- frontend/src/components/Whiteboard.jsx
+++ frontend/src/components/Whiteboard.jsx
@@ -1,35 +1,35 @@
-// import { Excalidraw } from "@excalidraw/excalidraw"
-// import "@excalidraw/excalidraw/index.css"
-
-// export default function Whiteboard() {
-//   return (
-//     <div style={{ height: "100%", width: "100%" }}>
-//       <Excalidraw
-//         UIOptions={{
-//           canvasActions: {
-//             loadScene: false,
-//             saveToActiveFile: false,
-//             export: false,
-//             saveAsImage: false
-//           }
-//         }}
-//       />
-//     </div>
-//   )
-// }
-import { Tldraw } from "tldraw"
-import "tldraw/tldraw.css"
+import { Excalidraw } from "@excalidraw/excalidraw"
+import "@excalidraw/excalidraw/index.css"
 
 export default function Whiteboard() {
   return (
-    <div className="relative w-full h-full">
-
-      <Tldraw
-        autoFocus
-        inferDarkMode
-        className="absolute inset-0"
+    <div style={{ height: "100%", width: "100%" }}>
+      <Excalidraw
+        UIOptions={{
+          canvasActions: {
+            loadScene: false,
+            saveToActiveFile: false,
+            export: false,
+            saveAsImage: false
+          }
+        }}
       />
-
     </div>
   )
 }
+// import { Tldraw } from "tldraw"
+// import "tldraw/tldraw.css"
+
+// export default function Whiteboard() {
+//   return (
+//     <div className="relative w-full h-full">
+
+//       <Tldraw
+//         autoFocus
+//         inferDarkMode
+//         className="absolute inset-0"
+//       />
+
+//     </div>
+//   )
+// }
